fdd6739e79d4570e127b944bd8e11f884d165a6d
Lukas Marx <marxlukas@googlemail.com>
2018-02-21 08:34:14 +0100

    disabled eslint for now added link to home to logo

--- src-nuxt/layouts/default.vue
+++ src-nuxt/layouts/default.vue
@@ -4,24 +4,25 @@
     
           <div class="col-xs-offset-0 col-sm-offset-1 col-md-offset-1 col-lg-offset-2 col-xs-12 col-sm-10 col-md-10 col-lg-8" style="height:100%">
             <div style="height:100%">
-            <svg heigth="56px" style="height:56px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-              viewBox="0 0 1000 220" xml:space="preserve">
-              <style type="text/css">
-                .st0{fill:#DD0031;}
-                .st1{fill:#C3002F;}
-                .st2{fill:#FFFFFF;}
-              </style>
-              <g transform="translate(0,50)">
-              <text x="0" y="125" font-family="Verdana" font-size="180" fill="#333">m</text>
-              <g transform="translate(150, -60)">
-                <polygon class="st0" points="125,30 125,30 125,30 31.9,63.2 46.1,186.3 125,230 125,230 125,230 203.9,186.3 218.1,63.2"/>
-                <polygon class="st1" points="125,30 125,52.2 125,52.1 125,153.4 125,153.4 125,230 125,230 203.9,186.3 218.1,63.2 125,30"/>
-              </g>
-              <text x="220" y="125" font-family="Verdana" font-size="180" fill="white">a</text>
-              <text x="380" y="125" font-family="Verdana" font-size="180" fill="#333">lcoded</text>
-              </g>
-            </svg>
-
+              <nuxt-link :to="{ path: '/'}" title="Blog">
+                <svg heigth="56px" style="height:56px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                  viewBox="0 0 1000 220" xml:space="preserve">
+                  <style type="text/css">
+                    .st0{fill:#DD0031;}
+                    .st1{fill:#C3002F;}
+                    .st2{fill:#FFFFFF;}
+                  </style>
+                  <g transform="translate(0,50)">
+                  <text x="0" y="125" font-family="Verdana" font-size="180" fill="#333">m</text>
+                  <g transform="translate(150, -60)">
+                    <polygon class="st0" points="125,30 125,30 125,30 31.9,63.2 46.1,186.3 125,230 125,230 125,230 203.9,186.3 218.1,63.2"/>
+                    <polygon class="st1" points="125,30 125,52.2 125,52.1 125,153.4 125,153.4 125,230 125,230 203.9,186.3 218.1,63.2 125,30"/>
+                  </g>
+                  <text x="220" y="125" font-family="Verdana" font-size="180" fill="white">a</text>
+                  <text x="380" y="125" font-family="Verdana" font-size="180" fill="#333">lcoded</text>
+                  </g>
+                </svg>
+              </nuxt-link>
               <div style="height:100%; float:right">
               <nuxt-link :to="{ path: '/'}" title="Blog">
                 <button aria-label="Blog" class="toolbar-button" flat>Blog</button>
@@ -67,135 +68,138 @@
 </template>
 <script>
 export default {
-  data() {
-    return {
-      snackbar: false,
-      y: 'bottom',
-      x: null,
-      mode: 'multi-line',
-      timeout: 0,
-      text:
-        'Cookies help to improve this Website! By using this site you agree, that we may save cookies on your device.'
-    };
-  },
-  mounted() {
-    if (typeof window !== 'undefined') {
-      let cookie = document.cookie;
+    data() {
+        return {
+            snackbar: false,
+            y: 'bottom',
+            x: null,
+            mode: 'multi-line',
+            timeout: 0,
+            text:
+                'Cookies help to improve this Website! By using this site you agree, that we may save cookies on your device.'
+        };
+    },
+    mounted() {
+        if (typeof window !== 'undefined') {
+            let cookie = document.cookie;
 
-      if (/consentCookie=true/.test(cookie)) {
-        this.snackbar = false;
-      } else {
-        this.snackbar = true;
-      }
+            if (/consentCookie=true/.test(cookie)) {
+                this.snackbar = false;
+            } else {
+                this.snackbar = true;
+            }
+        }
+    },
+    methods: {
+        consent() {
+            if (document) {
+                let now = new Date().getTime();
+                const date = new Date((now += 86400000 * 365));
+                document.cookie =
+                    'consentCookie=true;expires=' +
+                    date.toUTCString() +
+                    ';path=/;';
+            }
+            this.snackbar = false;
+        }
     }
-  },
-  methods: {
-    consent() {
-      if (document) {
-        let now = new Date().getTime();
-        const date = new Date((now += 86400000 * 365));
-        document.cookie = 'consentCookie=true;expires=' + date.toUTCString() + ';path=/;';
-      }
-      this.snackbar = false;
-    }
-  }
 };
 </script>
 <style>
 html {
-  font-family: Roboto, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-  color: #333 !important;
+    font-family: Roboto, sans-serif;
+    font-size: 16px;
+    word-spacing: 1px;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    box-sizing: border-box;
+    color: #333 !important;
 }
 .snackbar {
-  position: fixed;
-  padding: 16px;
-  bottom: 0;
-  left: 50%;
-  transform: translate(-50%, 0);
-  right: auto;
-  margin-left: auto;
-  margin-right: auto;
-  background-color: #333;
-  color: white;
+    position: fixed;
+    padding: 16px;
+    bottom: 0;
+    left: 50%;
+    transform: translate(-50%, 0);
+    right: auto;
+    margin-left: auto;
+    margin-right: auto;
+    background-color: #333;
+    color: white;
 }
 @media screen and (max-width: 960px) {
-  .snackbar {
-    width: 100%;
-  }
+    .snackbar {
+        width: 100%;
+    }
 }
 
 .col {
-  padding-left: 4px !important;
-  padding-right: 4px !important;
+    padding-left: 4px !important;
+    padding-right: 4px !important;
 }
 
 .default-toolbar {
-  height: 56px;
-  background-color: white;
-  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-  width: 100%;
+    height: 56px;
+    background-color: white;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+    width: 100%;
 }
 
 .default-container {
-  width: 100%;
+    width: 100%;
 }
 
 .outer-container {
-  min-height: 100vh;
-  background-color: #f5f5f5;
+    min-height: 100vh;
+    background-color: #f5f5f5;
 }
 
 .icon-color {
-  color: #333;
+    color: #333;
 }
 
 .icon-color:hover {
-  color: #c3002f;
+    color: #c3002f;
 }
 
 .large-icon {
-  font-size: 64px;
+    font-size: 64px;
 }
 .grey--text {
-  color: #999;
+    color: #999;
 }
 
 button {
-  color: inherit;
-  background-color: transparent;
-  outline: none;
-  border-width: 0;
-  transition: background-color 0.3s;
-  padding: 16px;
-  font-weight: 700;
+    color: inherit;
+    background-color: transparent;
+    outline: none;
+    border-width: 0;
+    transition: background-color 0.3s;
+    padding: 16px;
+    font-weight: 700;
 }
 
 button:hover {
-  background-color: #ddd !important;
+    background-color: #ddd !important;
 }
 
 .toolbar-button {
-  height: 56px;
-  margin: 0 !important;
+    height: 56px;
+    margin: 0 !important;
 }
 
 .toolbar-button:hover {
-  background-color: #ddd !important;
+    background-color: #ddd !important;
 }
 
 a {
-  text-decoration: none !important;
-  color: inherit;
+    text-decoration: none !important;
+    color: inherit;
 }
 
 a:hover {
-  text-decoration: none !important;
+    text-decoration: none !important;
 }
 </style>
--- src-nuxt/pages/index.vue
+++ src-nuxt/pages/index.vue
@@ -63,123 +63,128 @@
 </template>
 
 <script>
-import { posts, angularBeginner, angularIntermediate } from '~/apollo/queries/posts';
+import {
+    posts,
+    angularBeginner,
+    angularIntermediate
+} from '~/apollo/queries/posts';
 import postcard from '~/components/postcard.vue';
 
 export default {
-  // layout: 'default',
-  components: { postcard },
-  apollo: {
-    posts: {
-      query: posts,
-      prefetch: true,
-      update: ({ BlogPosts }) => {
-        return BlogPosts;
-      }
+    // layout: 'default',
+    components: { postcard },
+    apollo: {
+        posts: {
+            query: posts,
+            prefetch: true,
+            update: ({ BlogPosts }) => {
+                return BlogPosts;
+            }
+        },
+        beginner: {
+            query: angularBeginner,
+            prefetch: true,
+            update: ({ BlogPosts }) => {
+                return BlogPosts;
+            }
+        },
+        intermediate: {
+            query: angularIntermediate,
+            prefetch: true,
+            update: ({ BlogPosts }) => {
+                return BlogPosts;
+            }
+        }
     },
-    beginner: {
-      query: angularBeginner,
-      prefetch: true,
-      update: ({ BlogPosts }) => {
-        return BlogPosts;
-      }
-    },
-    intermediate: {
-      query: angularIntermediate,
-      prefetch: true,
-      update: ({ BlogPosts }) => {
-        return BlogPosts;
-      }
-    }
-  },
-  methods: {
-    first2: posts => {
-      if (!posts) return null;
-      return posts.slice(0, 2).map((value, index) => {
-        const clone = JSON.parse(JSON.stringify(value));
+    methods: {
+        first2: posts => {
+            if (!posts) return null;
+            return posts.slice(0, 2).map((value, index) => {
+                const clone = JSON.parse(JSON.stringify(value));
 
-        const description = clone.description.substring(0, 197);
-        if (clone.description.length > 200) {
-          clone.description = description + '...';
-        } else {
-          clone.description = description;
-        }
+                const description = clone.description.substring(0, 197);
+                if (clone.description.length > 200) {
+                    clone.description = description + '...';
+                } else {
+                    clone.description = description;
+                }
 
-        return clone;
-      });
-    },
-    second3: posts => {
-      if (!posts) return null;
-      return posts.slice(2, posts.length).map((value, index) => {
-        const clone = JSON.parse(JSON.stringify(value));
-        const description = clone.description.substring(0, 197);
-        if (clone.description.length > 200) {
-          clone.description = description + '...';
-        } else {
-          clone.description = description;
+                return clone;
+            });
+        },
+        second3: posts => {
+            if (!posts) return null;
+            return posts.slice(2, posts.length).map((value, index) => {
+                const clone = JSON.parse(JSON.stringify(value));
+                const description = clone.description.substring(0, 197);
+                if (clone.description.length > 200) {
+                    clone.description = description + '...';
+                } else {
+                    clone.description = description;
+                }
+                return clone;
+            });
+        },
+        all: posts => {
+            if (!posts) return null;
+            return posts.map((value, index) => {
+                const clone = JSON.parse(JSON.stringify(value));
+                const description = clone.description.substring(0, 197);
+                if (clone.description.length > 200) {
+                    clone.description = description + '...';
+                } else {
+                    clone.description = description;
+                }
+                return clone;
+            });
         }
-        return clone;
-      });
-    },
-    all: posts => {
-      if (!posts) return null;
-      return posts.map((value, index) => {
-        const clone = JSON.parse(JSON.stringify(value));
-        const description = clone.description.substring(0, 197);
-        if (clone.description.length > 200) {
-          clone.description = description + '...';
-        } else {
-          clone.description = description;
-        }
-        return clone;
-      });
     }
-  }
 };
 </script>
 
 <style>
 .index-container {
-  margin-top: 32px;
+    margin-top: 32px;
 }
 .container {
-  min-height: 100vh;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+    min-height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
 }
 .section-title {
-  margin-top: 64px;
-  margin-bottom: 16px;
-  float: left;
+    margin-top: 64px;
+    margin-bottom: 16px;
+    float: left;
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-    'Helvetica Neue', Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system,
+        BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
+        sans-serif; /* 1 */
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
 }
 
 .subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
 }
 
 .links {
-  padding-top: 15px;
+    padding-top: 15px;
 }
 a {
-  color: inherit;
+    color: inherit;
 }
 
 a:hover {
-  color: inherit;
+    color: inherit;
 }
 </style>
