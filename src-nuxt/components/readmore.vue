19190c9c9cc424303087ac7a44e623017da832e5
Lukas Marx <marxlukas@googlemail.com>
2018-03-25 15:48:33 +0200

    fixing null error on nodes

--- src-nuxt/components/readmore.vue
+++ src-nuxt/components/readmore.vue
@@ -2,7 +2,7 @@
   <div class="rm-container">
       <h1 class="rm-title">Read More!</h1>
         <div
-            v-for="post in BlogPosts.nodes"
+            v-for="post in BlogPosts ? BlogPosts.nodes: []"
             :key="post.title"
             class="rm-layout col-md-4 col"
         >
