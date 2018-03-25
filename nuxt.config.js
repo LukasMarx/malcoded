c14179ef555f5edd6c8e99d74f72d87d7d5dc59f
Lukas Marx <marxlukas@googlemail.com>
2018-03-25 16:43:55 +0200

    removed material icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ module.exports = {
         ],
         link: [
             { rel: 'icon', type: 'image/x-icon', href: '/_nuxt/favicon.ico' },
-            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
+            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700' }
         ]
     },
     /*
