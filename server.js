0b3606dc1fb5f25913a541138af53261baa8dca2
Lukas Marx <marxlukas@googlemail.com>
2018-04-06 18:29:53 +0200

    changed feed library to "rss"

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const { Nuxt, Builder } = require('nuxt');
-const Feed = require('feed');
+const RSS = require('rss');
 const request = require('graphql-request').request;
+
+const feedOptions = {
+    title: 'malcoded',
+    description: 'Angular, Typescript & more',
+    feed_url: 'https://malcoded.com/rss',
+    site_url: 'https://malcoded.com',
+    managingEditor: 'malcoded.blog@gmail.com (Lukas Marx)',
+    language: 'en'
+};
+const feed = new RSS(feedOptions);
 
 let nuxtConfig = require('./nuxt.config.js');
 nuxtConfig.dev = false;
@@ -14,51 +24,30 @@ const query = `{
             thumbnail
             description
             url
+            releaseDate
         }
     }
   }`;
 
 app.get('/rss', (req, res) => {
-    let feed = new Feed({
-        title: 'malcoded | Angular, Typescript & more!',
-        description: 'malcoded | Angular, Typescript & more!',
-        id: 'https://malcoded.com/',
-        link: 'https://malcoded.com/',
-        favicon: 'https://malcoded.com/favicon.ico',
-        generator: 'malcoded', // optional, default = 'Feed for Node.js'
-        feedLinks: {
-            json: 'https://example.com/json',
-            atom: 'https://example.com/atom'
-        },
-        author: {
-            name: 'Lukas Marx',
-            email: 'malcoded.blog@gmail.com',
-            link: 'https://malcoded.com'
-        }
-    });
-
     request('https://malcoded.com/api/v1/48238e83-87dd-4b4f-be48-26ea7c89e8e7/api', query).then(
         data => {
             if (data && data.BlogPosts && data.BlogPosts.nodes) {
                 data.BlogPosts.nodes.forEach(node => {
-                    feed.addItem({
+                    const itemOptions = {
                         title: node.title,
-                        id: node.url,
-                        link: 'https://malcoded.com/posts/' + node.url,
                         description: node.description,
-                        //content: post.content,
-                        author: {
-                            name: 'Lukas Marx',
-                            email: 'malcoded.blog@gmail.com',
-                            link: 'https://malcoded.com'
-                        },
-                        date: node.releaseDate,
-                        image: 'https://malcoded.com/api/v1/48238e83-87dd-4b4f-be48-26ea7c89e8e7/asset/' + node.thumbnail + '/jpg'
-                    });
+                        url: 'https://malcoded.com/posts/' + node.url,
+                        guid: 'https://malcoded.com/posts/' + node.url,
+                        enclosure: { url: 'https://malcoded.com/api/v1/48238e83-87dd-4b4f-be48-26ea7c89e8e7/asset/' + node.thumbnail + '/jpg' },
+                        author: 'Lukas Marx',
+                        date: node.releaseDate
+                    };
+                    feed.item(itemOptions);
                 });
             }
             res.set('Content-Type', 'text/xml');
-            res.send(feed.rss2());
+            res.send(feed.xml({ indent: true }));
         },
         err => {
             res.sendStatus(404);
--- src-nuxt/static/sw.js
+++ src-nuxt/static/sw.js
@@ -8,44 +8,48 @@ const workboxSW = new self.WorkboxSW({
 
 workboxSW.precache([
   {
-    "url": "/dist/app.841ef176885920264f53435538513eec.css",
-    "revision": "841ef176885920264f53435538513eec"
+    "url": "/dist/app.2506ae895ea296ffa1de.js",
+    "revision": "db697fbb30c01ebae835a9241025510b"
   },
   {
-    "url": "/dist/app.a38334247df1731164fc.js",
-    "revision": "dc73244fc7c916540db1cbaab81087aa"
+    "url": "/dist/app.c3b1211ca03346a7f185fb7dbd03112a.css",
+    "revision": "c3b1211ca03346a7f185fb7dbd03112a"
   },
   {
-    "url": "/dist/layouts_default.2968aefa739a22ad830c.js",
-    "revision": "cb9df0ed0a941305dd9103ced991fa39"
+    "url": "/dist/layouts_default.88145e16a89bcdce19ca.js",
+    "revision": "0f7ae522fbfb43a1cafb0731abe22ff7"
   },
   {
-    "url": "/dist/manifest.da6de3ec6abbf5756045.js",
-    "revision": "7dc0e51e8224ccb4df2ee4c7c799ba3d"
+    "url": "/dist/manifest.5a02d6176d664f0aa031.js",
+    "revision": "ce93271a48fef088efdcd0903fab8ecf"
   },
   {
-    "url": "/dist/pages_aboutme.06e7303d726cc88285cc.js",
-    "revision": "72e011447b4c917be8329069fc94e24c"
+    "url": "/dist/pages_aboutme.afbe438ef053140b2787.js",
+    "revision": "2c1effadc7ef89217b99bebeafd7ce78"
   },
   {
-    "url": "/dist/pages_category__category.433dcec48895714c56da.js",
-    "revision": "2971b94ae3d4e6518ee6f57ac2d122ce"
+    "url": "/dist/pages_category__category.80c3d9d18e87f486de24.js",
+    "revision": "8ccc9180bacb8a97c68858f0966ff540"
   },
   {
-    "url": "/dist/pages_index.286fe08c5b1e8297feed.js",
-    "revision": "9f7891444b1e191999b613b1b32a5e91"
+    "url": "/dist/pages_index.cccae81eb13f8c899576.js",
+    "revision": "f3e85574875383459202c275873d9763"
   },
   {
-    "url": "/dist/pages_legal.562997654ccfd55422fa.js",
-    "revision": "e928caf29047cf5f21e3dadb81c7406e"
+    "url": "/dist/pages_legal.789229c96242a0ad2fa0.js",
+    "revision": "36685c8f43aab386a86a5d74110fca93"
   },
   {
-    "url": "/dist/pages_posts__post.1331dbc9a54fe87026d2.js",
-    "revision": "bb2dd44bce1c5c11a0e47eb93830f4e2"
+    "url": "/dist/pages_posts__post.f6d8a21656b2ad265853.js",
+    "revision": "7105b2fb99247f0b0a474899d3fa80fc"
   },
   {
-    "url": "/dist/vendor.dc5c2b7b994fc83e21d0.js",
-    "revision": "4846a01cf23baac95f44561803800690"
+    "url": "/dist/vendor.8079cb6e83ef1342348e.js",
+    "revision": "9717755f1910318743475f740c341b84"
+  },
+  {
+    "url": "/dist/vendor.a8dc81343336b6986b7592f96f5c9b44.css",
+    "revision": "a8dc81343336b6986b7592f96f5c9b44"
   }
 ])
 
