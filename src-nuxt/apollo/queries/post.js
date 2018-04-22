7be2f57713608295b3509544976f75463148ed45
Lukas Marx <marxlukas@googlemail.com>
2018-04-22 17:37:33 +0200

    fixed: suggestion linking to invalid page

--- src-nuxt/apollo/queries/post.js
+++ src-nuxt/apollo/queries/post.js
@@ -1,5 +1,5 @@
 import gql from 'graphql-tag';
 
 export default gql(
-  'query Post($filter: [FilterInput]!) {BlogPost(filter: $filter) {title\n thumbnail\n description\n url\n content\n releaseDate\n recommendations(limit: 3) {\nnodes {title\n thumbnail}} primaryColor }}'
+  'query Post($filter: [FilterInput]!) {BlogPost(filter: $filter) {title\n thumbnail\n description\n url\n content\n releaseDate\n recommendations(limit: 3) {\nnodes {title\n thumbnail\n url}} primaryColor }}'
 );
