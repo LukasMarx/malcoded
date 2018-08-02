6e7a69521772dc3330113705a6477528e584fb0f
Lukas Marx <marxlukas@googlemail.com>
2018-08-02 12:00:29 +0200

    first custom ws impl

--- src-nuxt/modules/analytics/plugin.js
+++ src-nuxt/modules/analytics/plugin.js
@@ -1,8 +1,20 @@
-import io from 'socket.io-client';
-const socket = io('https://malcoded.com', { path: '/analytics' });
+const socket = new WebSocket('ws://localhost:3001/analytics');
+
+const open = new Promise((resolve, reject) => {
+  socket.onopen = () => {
+    socket.send(JSON.stringify({ messageType: 'joinUser' }));
+    resolve(socket);
+  };
+});
 
 export default ({ app: { router } }) => {
-  router.afterEach((to, from) => {
-    socket.emit('navigation', { projectId: '48238e83-87dd-4b4f-be48-26ea7c89e8e7', path: to.path });
+  open.then(() => {
+    console.log(router);
+    socket.send(
+      JSON.stringify({ messageType: 'navigation', payload: { projectId: '48238e83-87dd-4b4f-be48-26ea7c89e8e7', path: router.history.current.path } })
+    );
+    router.afterEach((to, from) => {
+      socket.send(JSON.stringify({ messageType: 'navigation', payload: { projectId: '48238e83-87dd-4b4f-be48-26ea7c89e8e7', path: to.path } }));
+    });
   });
 };
