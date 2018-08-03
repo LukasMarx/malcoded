9bff874e8e240d1502475c6234493103290fb81d
Lukas Marx <marxlukas@googlemail.com>
2018-08-03 08:29:46 +0200

    removed all analytics code

--- src-nuxt/modules/analytics/analytics.js
+++ src-nuxt/modules/analytics/analytics.js
@@ -1,9 +1,9 @@
-const { resolve } = require('path');
+// const { resolve } = require('path');
 
-module.exports = function module(moduleOptions) {
-  this.addPlugin({
-    src: resolve(__dirname, 'plugin.js'),
-    ssr: false,
-    options: moduleOptions
-  });
-};
+// module.exports = function module(moduleOptions) {
+//   this.addPlugin({
+//     src: resolve(__dirname, 'plugin.js'),
+//     ssr: false,
+//     options: moduleOptions
+//   });
+// };
--- src-nuxt/modules/analytics/plugin.js
+++ src-nuxt/modules/analytics/plugin.js
@@ -1,20 +1,20 @@
-const socket = new WebSocket('wss://malcoded.com/analytics');
+// const socket = new WebSocket('wss://malcoded.com/analytics');
 
-const open = new Promise((resolve, reject) => {
-  socket.onopen = () => {
-    socket.send(JSON.stringify({ messageType: 'joinUser' }));
-    resolve(socket);
-  };
-});
+// const open = new Promise((resolve, reject) => {
+//   socket.onopen = () => {
+//     socket.send(JSON.stringify({ messageType: 'joinUser' }));
+//     resolve(socket);
+//   };
+// });
 
-export default ({ app: { router } }) => {
-  open.then(() => {
-    console.log(router);
-    socket.send(
-      JSON.stringify({ messageType: 'navigation', payload: { projectId: '48238e83-87dd-4b4f-be48-26ea7c89e8e7', path: router.history.current.path } })
-    );
-    router.afterEach((to, from) => {
-      socket.send(JSON.stringify({ messageType: 'navigation', payload: { projectId: '48238e83-87dd-4b4f-be48-26ea7c89e8e7', path: to.path } }));
-    });
-  });
-};
+// export default ({ app: { router } }) => {
+//   open.then(() => {
+//     console.log(router);
+//     socket.send(
+//       JSON.stringify({ messageType: 'navigation', payload: { projectId: '48238e83-87dd-4b4f-be48-26ea7c89e8e7', path: router.history.current.path } })
+//     );
+//     router.afterEach((to, from) => {
+//       socket.send(JSON.stringify({ messageType: 'navigation', payload: { projectId: '48238e83-87dd-4b4f-be48-26ea7c89e8e7', path: to.path } }));
+//     });
+//   });
+// };
