23430d083905a43b12fb024e47dc202d27c89b80
Lukas Marx <marxlukas@googlemail.com>
2018-10-12 15:02:48 +0200

    fixed social media meta image url

--- src-nuxt/pages/posts/_post.vue
+++ src-nuxt/pages/posts/_post.vue
@@ -130,7 +130,7 @@ export default {
         {
           hid: 'og:image',
           property: 'og:image',
-          content: 'https://malcoded.com/api/v1/48238e83-87dd-4b4f-be48-26ea7c89e8e7/asset/' + post.thumbnail + '/jpg'
+          content: assetUrl + post.thumbnail + '.jpg'
         },
         {
           hid: 'apple-mobile-web-app-title',
